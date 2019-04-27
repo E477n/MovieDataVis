33bf32e216d236734c8e9e09ac859dd2fe4eaff7
merestnora <merestnora@outlook.com>
2019-04-27 16:55:41 +0800

    factor 3

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # MovieDataVis
-## Python Dependencies
+## Dependencies
 - dash
 - pymongo
 - pyquery
 - statsmodels
+## Introduction
+A mathematical model for the decision making of movie releasing date. 
+And the visualizations of movie data have also been done to provide a better cooperation between human decision and machine decision. 
---
+++ dataProcess/factor3.py
@@ -0,0 +1,38 @@
+import pymongo
+import datetime
+
+client = pymongo.MongoClient(host='127.0.0.1', port=27017)
+db = client['movie_db']
+
+col_gd = db['genre_details']
+
+def updateWeek():
+    for record in col_gd.find():
+        id = record['_id']
+        rd = record['Date']
+        rd = rd.split('/')
+        print(rd)
+        if 0 <= int(rd[2]) <= 18:
+            rd[2] = "20" + rd[2]
+        else:
+            rd[2] = "19" + rd[2]
+        print(rd)
+        dt = datetime.date(int(rd[2]), int(rd[0]), int(rd[1]))
+        wk = dt.isocalendar()[1]
+        print(wk)
+        col_gd.find_one_and_update({"_id": id},
+                            {"$set": {"week#": wk}})
+
+def calTotalGrossofSameGenreSameWeek(weekset, genre):
+    res = []
+    for week in weekset:
+        weeksum = 0
+        for record in col_gd.find({"week#": week}):
+            if record["Genre"] in genre:
+                weeksum += record["OpeningGross($)"]
+        res.append(weeksum)
+    print(res)
+
+if __name__ == "__main__":
+    # updateWeek()
+    calTotalGrossofSameGenreSameWeek([12, 13, 14, 15, 16, 17], "Animation")
